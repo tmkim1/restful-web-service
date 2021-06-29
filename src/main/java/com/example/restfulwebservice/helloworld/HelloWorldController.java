274634725eb71683f81d29035ecb5e6519b5c8ea
kimtaemin <xoalstjtm@gmail.com>
2021-06-29 18:56:39 +0900

    다국어 처리를 위한 internationalization 구현

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.example.restfulwebservice.helloworld;
 
 import com.example.restfulwebservice.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired //어노테이션을 통한 주입
+    private MessageSource messageSource;
+
     // GET
     // /hello-world (호출하는 endpoint)
     // RequestMapping(method=RequestMethod.GET, path="hello-world")
@@ -23,6 +32,11 @@ public class HelloWorldController {
     @GetMapping(path = "hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format( "Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 
 }
