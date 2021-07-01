11ccd312b838878f1296f0ce6392afabb23c85a4
kimtaemin <xoalstjtm@gmail.com>
2021-07-01 17:03:30 +0900

    REST API Monitoring을 위한 Actuator 설정

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -2,16 +2,39 @@ package com.example.restfulwebservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    //srpingfox.documentation.service.Contact
+    private static final Contact DEFAULT_CONTACT = new Contact("Taemin Kim",
+            "http:www.naver.com", "xxoals1021@naver.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awsome API title",
+            "My User management REST API service", "1.0", "urn:tos",
+            DEFAULT_CONTACT, "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json", "application/xml")); //Arrays.asList (배열을 리스트 형태로 변환)
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+
     }
 }
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +18,21 @@ import java.util.Date;
 @NoArgsConstructor // 디펄트 생성자 구현
 //@JsonIgnoreProperties(value={"password","ssn"})
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해주세요")
+    @ApiModelProperty(notes = "사용자 이름을 입력해주세요.")
     private String name;
     @Past // 과거 날짜만 사용 가능
+    @ApiModelProperty(notes = "사용자 등록일을 입력해주세요.")
     private Date joinDate;
 
     //@JsonIgnore //response 반환 컬럼 항목에서 제외, 위에 처럼 클래스 위에 JsonIgoreProperties로 일괄 처리 가능.
+    @ApiModelProperty(notes = "사용자 암호를 입력해주세요.")
     private String password;
     //@JsonIgnore
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해주세요.")
     private String ssn;
 }
