a2ccdaab6bb7a691e3e73f45fc640b0dec6a0be2
kimtaemin <xoalstjtm@gmail.com>
2021-07-02 19:25:58 +0900

    JPA POST/DELETE

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values(1, sysdate(), 'User1', 'test1111', '701010-1111111');
-insert into user values(2, sysdate(), 'User2', 'test2222', '701011-1111111');
-insert into user values(3, sysdate(), 'User3', 'test3333', '701012-1111111');
+insert into user values(10001, sysdate(), 'User1', 'test1111', '701010-1111111');
+insert into user values(10002, sysdate(), 'User2', 'test2222', '701011-1111111');
+insert into user values(10003, sysdate(), 'User3', 'test3333', '701012-1111111');
