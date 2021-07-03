d164597bfcc18f0bfc887c7af8aa9a35eedae3ae
kimtaemin <xoalstjtm@gmail.com>
2021-07-03 21:56:54 +0900

    게시물 관리를 위한 Post Entity 추가와 초기 데이터 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into user values(10001, sysdate(), 'User1', 'test1111', '701010-1111111');
 insert into user values(10002, sysdate(), 'User2', 'test2222', '701011-1111111');
 insert into user values(10003, sysdate(), 'User3', 'test3333', '701012-1111111');
+
+insert into post values(90001, 'My first post', 10001);
+insert into post values(90002, 'My second post', 10002);
